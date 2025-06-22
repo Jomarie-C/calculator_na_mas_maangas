8fb7fe6bf8933046fd59dce47884084b9e24af5a
unknown <cagurunganjomarie6@gmail.com>
2025-06-23 00:16:11 +0800

    Initializing variables

--- button_handling.py
+++ button_handling.py
@@ -1,2 +1,7 @@
 from calculator_core import Calculator
 from utility_function import format_number
+
+class ButtonClickHandler:
+    def __init__(self, display_label):
+        self.display_label = display_label
+        self.calculator = Calculator()
