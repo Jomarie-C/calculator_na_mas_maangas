963ae3ae29ed3a468680786e8f764df13df1da9f
unknown <cagurunganjomarie6@gmail.com>
2025-06-23 00:55:27 +0800

    Fixing also the formatting issue and square root handling

--- button_handling.py
+++ button_handling.py
@@ -1,5 +1,6 @@
+import math
 from calculator_core import Calculator
-from utility_function import format_number
+from utility_function import Format
 
 class ButtonClickHandler:
     def __init__(self, display_label):
@@ -11,7 +12,16 @@ class ButtonClickHandler:
             if button_value == "=":
                 self.calculator.second_operand = self.display_label["text"]
                 result = self.calculator.calculate_result()
-                self.display_label["text"] = format_number(result)
+                if isinstance(result, str):
+                    self.display_label["text"] = result
+                else:
+                    self.display_label["text"] = Format.format_number(result)
+            elif button_value == "√":
+                try:
+                    square_root = math.sqrt(float(self.display_label["text"]))
+                    self.display_label["text"] = Format.format_number(square_root)
+                except ValueError:
+                    self.display_label["text"] = "Error"
             else:
                 self.calculator.first_operand = self.display_label["text"]
                 self.calculator.operator = button_value
@@ -22,11 +32,17 @@ class ButtonClickHandler:
                 self.calculator.reset()
                 self.display_label["text"] = "0"
             elif button_value == "+/-":
-                inverted = float(self.display_label["text"]) * -1
-                self.display_label["text"] = format_number(inverted)
+                try:
+                    inverted = float(self.display_label["text"]) * -1
+                    self.display_label["text"] = Format.format_number(inverted)
+                except ValueError:
+                    self.display_label["text"] = "Error"
             elif button_value == "%":
-                percent = float(self.display_label["text"]) / 100
-                self.display_label["text"] = format_number(percent)
+                try:
+                    percentage = float(self.display_label["text"]) / 100
+                    self.display_label["text"] = Format.format_number(percentage)
+                except ValueError:
+                    self.display_label["text"] = "Error"
 
         elif button_value == ".":
             if "." not in self.display_label["text"]:
