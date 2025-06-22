8535f29a90c291ca5bb5dfcfa84516bcbb1ad25f
unknown <cagurunganjomarie6@gmail.com>
2025-06-23 00:06:43 +0800

    Serves as the foundation of the program

---
+++ calculator_core.py
@@ -0,0 +1,7 @@
+class Calculator:
+    def __init__(self):
+        self.first_operand = "0"
+        self.second_operand = None
+        self.operator = None
+        self.operation_symbols = ["÷", "×", "-", "+", "="]
+        self.control_symbols = ["AC", "+/-", "%"]
