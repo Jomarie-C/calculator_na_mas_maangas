c4d92c40cee6fa1cd049057dd7c591c06a8f8435
unknown <cagurunganjomarie6@gmail.com>
2025-06-23 00:09:37 +0800

    Continuation of the calculator's logic

--- calculator_core.py
+++ calculator_core.py
@@ -5,3 +5,24 @@ class Calculator:
         self.operator = None
         self.operation_symbols = ["÷", "×", "-", "+", "="]
         self.control_symbols = ["AC", "+/-", "%"]
+
+    def reset(self):
+        self.first_operand = "0"
+        self.second_operand = None
+        self.operator = None
+
+    def calculate_result(self):
+        try:
+            operand1 = float(self.first_operand)
+            operand2 = float(self.second_operand)
+
+            if self.operator == "+":
+                return operand1 + operand2
+            elif self.operator == "-":
+                return operand1 - operand2
+            elif self.operator == "×":
+                return operand1 * operand2
+            elif self.operator == "÷":
+                return operand1 / operand2
+        finally:
+            self.reset()
