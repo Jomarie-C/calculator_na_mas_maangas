8d969895019e65f54e2876f0da56bc6c2c46b4a1
unknown <cagurunganjomarie6@gmail.com>
2025-06-23 00:20:20 +0800

    Creating display buttons

--- button_handling.py
+++ button_handling.py
@@ -5,3 +5,35 @@ class ButtonClickHandler:
     def __init__(self, display_label):
         self.display_label = display_label
         self.calculator = Calculator()
+    
+    def handle_click(self, button_value):
+        if button_value in self.calculator.operation_symbols:
+            if button_value == "=":
+                self.calculator.second_operand = self.display_label["text"]
+                result = self.calculator.calculate_result()
+                self.display_label["text"] = format_number(result)
+            else:
+                self.calculator.first_operand = self.display_label["text"]
+                self.calculator.operator = button_value
+                self.display_label["text"] = "0"
+
+        elif button_value in self.calculator.control_symbols:
+            if button_value == "AC":
+                self.calculator.reset()
+                self.display_label["text"] = "0"
+            elif button_value == "+/-":
+                inverted = float(self.display_label["text"]) * -1
+                self.display_label["text"] = format_number(inverted)
+            elif button_value == "%":
+                percent = float(self.display_label["text"]) / 100
+                self.display_label["text"] = format_number(percent)
+
+        elif button_value == ".":
+            if "." not in self.display_label["text"]:
+                self.display_label["text"] += "."
+
+        elif button_value.isdigit():
+            if self.display_label["text"] == "0":
+                self.display_label["text"] = button_value
+            else:
+                self.display_label["text"] += button_value
